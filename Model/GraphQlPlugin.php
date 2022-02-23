c38dc344f6bac076ae4ff9d5f01f50be78c9edaa
Domagoj Potkoc <domagoj@favicode.net>
2022-02-23 11:31:19 +0100

    POST method - set raw content

--- Model/GraphQlPlugin.php
+++ Model/GraphQlPlugin.php
@@ -42,9 +42,7 @@ class GraphQlPlugin
                 if ($request->isGet()) {
                     $request->setParam('query', $fastlyHeaderQuery);
                 } elseif ($request->isPost()) {
-                    $request->setContent(
-                        json_encode(['query' => $fastlyHeaderQuery])
-                    );
+                    $request->setContent($fastlyHeaderQuery);
                 }
             }
         }
